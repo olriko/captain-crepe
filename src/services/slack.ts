7f0c8feeff843d85cff4e30613946a9d8db42a06
Olivier HAMON <mail@olriko.com>
2019-01-13 19:47:16 +0100

    [Slack] Add param for @here to be parsed by slack

--- src/services/slack.ts
+++ src/services/slack.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 export const notification =  async (text: string) => {
-    console.log(process.env)
     if (process.env.VUE_APP_SLACK_HOOK) {
         try {
             await axios.post(process.env.VUE_APP_SLACK_HOOK, {
                 username: 'Captain Haddock',
                 text,
+                link_names: true,
             }, {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
